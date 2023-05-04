93632c531b6ae9a5aba6d5d157889e90fe2c62f0
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-05-04 12:24:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,85 +1,147 @@
-const quizz = [
-  {
-    question: "Quel est le nom de la plus grande lune de Saturne ?",
-    reponses: ["lo", "Ganyméde", "titan"],
-    bonneReponse: "titan",
-  },
-  {
-    question: "Combien de remplacent y a-t-il dans une équipe de football ?",
-    reponses: ["5", "9", "11"],
-    bonneReponse: "9",
-  },
-  {
-    question: "Quel célèbre parc se trouve au cœur de Manhattan ?",
-    reponses: ["Hyde Park", "Central park", "Park Asterix"],
-    bonneReponse: "Central park",
-  },
-  {
-    question:
-      "Quel est le langage de programmation utilisé pour créer des sites web interactifs ?",
-    reponses: ["Javascript", "HTML", "Python"],
-    bonneReponse: "Javascript",
-  },
-  {
-    question:
-      "Quel acteur a joué le rôle de Walter White dans la série télévisée Breaking Bad ?",
-    reponses: ["Bob Odenkirk", "Aaron Paul", "Bryan Cranston"],
-    bonneReponse: "Bryan Cranston",
-  },
-];
+function init() {
+  const quizz = [
+    {
+      question: "Quel est le nom de la plus grande lune de Saturne ?",
+      reponses: ["lo", "Ganyméde", "titan"],
+      bonneReponse: "titan",
+    },
+    {
+      question: "Combien de remplacent y a-t-il dans une équipe de football ?",
+      reponses: ["5", "9", "11"],
+      bonneReponse: "9",
+    },
+    {
+      question: "Quel célèbre parc se trouve au cœur de Manhattan ?",
+      reponses: ["Hyde Park", "Central park", "Park Asterix"],
+      bonneReponse: "Central park",
+    },
+    {
+      question:
+        "Quel est le langage de programmation utilisé pour créer des sites web interactifs ?",
+      reponses: ["Javascript", "HTML", "Python"],
+      bonneReponse: "Javascript",
+    },
+    {
+      question:
+        "Quel acteur a joué le rôle de Walter White dans la série télévisée Breaking Bad ?",
+      reponses: ["Bob Odenkirk", "Aaron Paul", "Bryan Cranston"],
+      bonneReponse: "Bryan Cranston",
+    },
+    {
+      question:
+        "Quel est le nom de l'océan qui borde la côte Est des Etats-Unis ?",
+      reponses: ["Océan Atlantique", "Océan Pacifique", "Océan Indien"],
+      bonneReponse: "Océan Atlantique",
+    },
+    {
+      question: "Quel est le nom de la plus haute montagne du monde ?",
+      reponses: ["Kilimandjaro", "Mont Everest", "Mont Blanc"],
+      bonneReponse: "Mont Everest",
+    },
+    {
+      question:
+        "Quel est le nom de l'empereur romain qui a construit le Colisée ?",
+      reponses: ["Auguste", "Néron", "Vespasien"],
+      bonneReponse: "Vespasien",
+    },
+    {
+      question: "Dans quelle ville se trouve la Sagrada Familia ?",
+      reponses: ["Barcelone", "Madrid", "Valence"],
+      bonneReponse: "Barcelone",
+    },
+    {
+      question: "Quel est le nom de la plus grande île du monde ?",
+      reponses: ["Australie", "Groenland", "Java"],
+      bonneReponse: "Groenland",
+    },
+  ];
 
-let numberQuestion = 0;
-let questionFrom = document.getElementById("question-form");
+  const btn = document.querySelectorAll("#btn"); // boutton reponse
+  const reponseOne = document.querySelector(".reponse1"); //reponse 1
+  const reponsetwo = document.querySelector(".reponse2"); //reponse 1
+  const reponseThree = document.querySelector(".reponse3"); //reponse 1
+  const questionActuel = document.getElementById("questionForm"); //La question du quizz
+  const submit = document.getElementById("send"); //btn submit
+  const container = document.querySelector(".container"); // le container du quizz
+  const containerResult = document.querySelector(".container-result"); // le container du result
+  const ourScore = document.querySelector(".our-score"); // le score en fin de partie
+  const btnReplay = document.getElementById("replay-btn"); //le btn rejouer
 
-let questionActuel = quizz[numberQuestion].question;
+  console.log(ourScore);
+  let i = 0;
+  let score = 0;
 
-questionFrom.innerHTML = quizz[numberQuestion].question;
+  reponseOne.innerHTML = quizz[i].reponses[0];
+  reponsetwo.innerHTML = quizz[i].reponses[1];
+  reponseThree.innerHTML = quizz[i].reponses[2];
+  questionActuel.textContent = quizz[i].question;
 
-reponse1.innerHTML = quizz[0].reponses[0];
-reponse2.innerHTML = quizz[0].reponses[1];
-reponse3.innerHTML = quizz[0].reponses[2];
+  /*Pour rejouer*/
+  btnReplay.addEventListener("click", () => {
+    i = 0;
+    score = 0;
+    reponseOne.innerHTML = quizz[i].reponses[0];
+    reponsetwo.innerHTML = quizz[i].reponses[1];
+    reponseThree.innerHTML = quizz[i].reponses[2];
+    questionActuel.textContent = quizz[i].question;
+    container.style.display = "grid";
+    containerResult.style.display = "none";
+    ourScore.innerHTML = "";
+    btn.forEach((otherBtn) => {
+      otherBtn.classList.remove("active");
+    });
+    init();
+    // btn.classList.remove("active");
+  });
 
-// btn.addEventListener("click", () => {
-//   numberQuestion++;
-//   quizz[numberQuestion].question;
-//   reponse1.innerHTML = quizz[numberQuestion].reponses[0];
-//   reponse2.innerHTML = quizz[numberQuestion].reponses[1];
-//   reponse3.innerHTML = quizz[numberQuestion].reponses[2];
-//   questionFrom.innerHTML = quizz[numberQuestion].question;
-// });
+  send.addEventListener("click", () => {
+    if (i === quizz.length - 1) {
+      container.style.display = "none";
+      containerResult.style.display = "grid";
 
-const response = [reponse1, reponse2, reponse3];
+      console.log("yess papa jeux de jambree");
+    }
 
-response.forEach((span) => {
-    span.addEventListener('click', (event) => {
-      // Récupérer l'élément actif et la couleur de fond sélectionnée
-      const activeSpan = document.querySelector('.active');
-      const selectedColor = 'green'; // Remplacer par la couleur de votre choix
-  
-      // Vérifier si l'élément cliqué est l'élément actif ou non
-      if (event.target !== activeSpan) {
-        // Réinitialiser la couleur de fond de tous les éléments
-        response.forEach((span) => {
-          span.style.backgroundColor = 'transparent';
-        });
-  
-        // Appliquer la couleur de fond sélectionnée à l'élément cliqué
-        event.target.style.backgroundColor = selectedColor;
-  
-        // Mettre à jour l'élément actif
-        activeSpan.classList.remove('active');
-        event.target.classList.add('active');
-      }
+    if (valeur == quizz[i].bonneReponse) {
+      score++;
+      console.log(score);
+      ourScore.innerHTML = `Voici votre score: ${score}/${quizz.length}`;
+      console.log(
+        "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+      );
+    } else if (score === 0) {
+      ourScore.innerHTML = `Voici votre score: 0/${quizz.length}`;
+    }
 
+    i++;
+    if (i < quizz.length) {
+      reponseOne.innerHTML = quizz[i].reponses[0];
+      reponsetwo.innerHTML = quizz[i].reponses[1];
+      reponseThree.innerHTML = quizz[i].reponses[2];
+      questionActuel.textContent = quizz[i].question;
+    }
 
-      numberQuestion++;
-      quizz[numberQuestion].question;
-      reponse1.innerHTML = quizz[numberQuestion].reponses[0];
-      reponse2.innerHTML = quizz[numberQuestion].reponses[1];
-      reponse3.innerHTML = quizz[numberQuestion].reponses[2];
-      questionFrom.innerHTML = quizz[numberQuestion].question;
+    btn.forEach((otherBtn) => {
+      otherBtn.classList.remove("active");
+    });
+    send.style.display = "none";
+  });
 
+  let valeur;
 
+  btn.forEach((btns) => {
+    btns.addEventListener("click", (event) => {
+      valeur = event.target.textContent;
+
+      btn.forEach((otherBtn) => {
+        if (otherBtn !== btn) {
+          otherBtn.classList.remove("active");
+          send.style.display = "flex";
+        }
+      });
+      btns.classList.add("active");
     });
   });
-    
+}
+
+init(); // appeler la fonction pour démarrer le script
